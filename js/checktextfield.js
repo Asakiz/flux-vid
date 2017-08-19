38ca549b64e67bb0a4a8a587c360a470f2879c91
ittapupu <asakizin@gmail.com>
2017-08-19 18:17:48 -0300

    now search works

--- js/checktextfield.js
+++ js/checktextfield.js
@@ -1,5 +1,4 @@
-function checkTextField(field) {
-
-  document.getElementById("error").innerText =
-    (field.value === "rodrigo") ? window.location.href ="index2.html" : "esse vídeo não existe!";
+function checkTextField() {
+	document.getElementById("inputText").innerText =
+    (inputText.value === "movimento") ? window.location.href ="index2.html" : "esse vídeo não existe!";
 }
