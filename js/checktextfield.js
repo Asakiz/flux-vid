ad88daf6cf061b0907c88f6058fc1c6494617c1a
ittapupu <asakizin@gmail.com>
2017-08-19 19:51:12 -0300

    delete some files

--- js/checktextfield.js
+++
@@ -1,4 +0,0 @@
-function checkTextField() {
-	document.getElementById("inputText").innerText =
-    (inputText.value === "movimento") ? window.location.href ="index2.html" : "esse vídeo não existe!";
-}
